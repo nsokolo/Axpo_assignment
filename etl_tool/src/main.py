0437cedc72c79c511ddaf0045bfb544aa3af1b3d
Natalia Sokolowska <natalia.sokolowska@predict.systems>
2023-01-20 10:50:05 +0100

    1. create etl_tool package 2. add requirements.txt 3. add etl executable

--- etl_tool/src/main.py
+++ etl_tool/src/main.py
@@ -3,12 +3,16 @@ from transform import transform_data
 from load import load_data
 from utils import DateRange
 
-date_range = DateRange(start_date='2023-01-01', end_date='2023-01-03')
-report = "wielkości_podstawowe"  # "generacja_mocy", "wielkości_podstawowe", 'kontrakty_godzinowe'
-extracted_data = extract_data(report=report, date_range=date_range)
-print(extracted_data)
-transformed_data = transform_data(report=report, data=extracted_data, date_range=date_range)
-print(transformed_data)
-load_data(data=transformed_data, report=report, date_range=date_range)
+
+def main(start_date: str, end_date: str, report: str):
+    date_range = DateRange(start_date=start_date, end_date=end_date)
+    extracted_data = extract_data(report=report, date_range=date_range)
+    transformed_data = transform_data(report=report, data=extracted_data, date_range=date_range)
+    load_data(data=transformed_data, report=report, date_range=date_range)
 
 
+if __name__ == "__main__":
+    start_day = '2023-01-01'
+    end_day = '2023-01-03'
+    report_name = "wielkości_podstawowe"  # "generacja_mocy", "wielkości_podstawowe", 'kontrakty_godzinowe'
+    main(start_date=start_day, end_date=end_day, report=report_name)
